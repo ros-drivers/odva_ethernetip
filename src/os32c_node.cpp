d2f3f158598d3529b604c49d360870a1c8f40bb6
Kareem Shehata <kshehata@clearpathrobotics.com>
2015-08-14 13:59:37 -0400

    Starting with Hello World

---
+++ src/os32c_node.cpp
@@ -0,0 +1,18 @@
+/**
+Software License Agreement (proprietary)
+
+\file      os32c_node.cpp
+\authors   Kareem Shehata <kshehata@clearpathrobotics.com>
+\copyright Copyright (c) 2015, Clearpath Robotics, Inc., All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, is not permitted without the
+express permission of Clearpath Robotics.
+*/
+
+#include <ros/ros.h>
+
+int main(int argc, char const *argv[])
+{
+  ROS_ERROR("HELLO WORLD!");
+  return 0;
+}
