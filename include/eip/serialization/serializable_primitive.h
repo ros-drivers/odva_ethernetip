8d5822a5bf4ab726b4ca2b931d5bd09971c6f1c6
Kareem Shehata <kshehata@clearpathrobotics.com>
2015-11-06 14:14:57 -0200

    Added a class for serializing and deserializing primitives as a shortcut for providing data to get and set attributes.

---
+++ include/eip/serialization/serializable_primitive.h
@@ -0,0 +1,93 @@
+/**
+Software License Agreement (proprietary)
+
+\file      serializable_primitive.h
+\authors   Kareem Shehata <kshehata@clearpathrobotics.com>
+\copyright Copyright (c) 2015, Clearpath Robotics, Inc., All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, is not permitted without the
+express permission of Clearpath Robotics.
+*/
+
+#ifndef EIP_SERIALIZABLE_PRIMITIVE_H
+#define EIP_SERIALIZABLE_PRIMITIVE_H
+
+#include "eip/serialization/serializable.h"
+#include "eip/serialization/writer.h"
+#include "eip/serialization/reader.h"
+
+namespace eip {
+namespace serialization {
+
+/**
+ * Serialization of a generic primitive class that can be serialized as just the
+ * in memory data as is.
+ */
+template <typename T>
+class SerializablePrimitive : public Serializable
+{
+public:
+
+  /// Data as the primitive type to be serialized or deserialized
+  T data;
+
+  /**
+   * Create a primitive with default values. Useful for deserialization.
+   */
+  SerializablePrimitive() { }
+
+  /**
+   * Create serializable version of the data given
+   * @param d data to use for serialization
+   */
+  SerializablePrimitive(const T& d) : data(d) { }
+
+  /**
+   * Length to be serialized is the memory size of the type given
+   * @return Total length in bytes to be serialized
+   */
+  virtual size_t getLength() const
+  {
+    return sizeof(T);
+  }
+
+  /**
+   * Serialize the data instance into the writer.
+   * @param writer Writer to use for serialization
+   * @return the writer again
+   * @throw std::length_error if the buffer is too small
+   */
+  virtual Writer& serialize(Writer& writer) const
+  {
+    writer.write(data);
+  }
+
+  /**
+   * Deserialization is always done with the length of the type in the template
+   * @param reader Reader to use for deserialization
+   * @param length Ignored
+   * @return the reader again
+   * @throw std::length_error if the given length is greater than allocated 
+   *   data buffer
+   */
+  virtual Reader& deserialize(Reader& reader, size_t length)
+  {
+    deserialize(reader);
+  }
+
+  /**
+   * Deserialize data into this buffer, using the size of the buffer already
+   * allocated
+   * @param reader Reader to use for deserialization
+   * @return the reader again
+   * @throw std::length_error if the buffer is overrun while deserializing
+   */
+  virtual Reader& deserialize(Reader& reader)
+  {
+    reader.read(data);
+  }
+};
+
+} // namespace serialization
+} // namespace eip
+#endif  // EIP_SERIALIZABLE_PRIMITIVE_H
---
+++ test/eip/serialization/serializable_primitive_test.cpp
@@ -0,0 +1,127 @@
+/**
+Software License Agreement (proprietary)
+
+\file      serializable_primitive_test.cpp
+\authors   Kareem Shehata <kshehata@clearpathrobotics.com>
+\copyright Copyright (c) 2015, Clearpath Robotics, Inc., All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, is not permitted without the
+express permission of Clearpath Robotics.
+*/
+
+#include <gtest/gtest.h>
+#include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
+#include <boost/asio.hpp>
+#include <boost/iostreams/stream.hpp>
+#include <boost/iostreams/device/array.hpp>
+
+#include "eip/serialization/buffer_writer.h"
+#include "eip/serialization/buffer_reader.h"
+#include "eip/serialization/serializable_primitive.h"
+
+using boost::shared_ptr;
+using boost::make_shared;
+using namespace boost::asio;
+using namespace boost::iostreams;
+using namespace eip::serialization;
+
+class SerializablePrimitiveTest : public :: testing :: Test
+{
+
+};
+
+TEST_F(SerializablePrimitiveTest, test_serialize_EIP_UDINT)
+{
+  shared_ptr<Serializable> sp = make_shared< SerializablePrimitive <EIP_UDINT> > (0xDEADBEEF);
+
+  unsigned char d[4]; 
+  BufferWriter writer(buffer(d));
+
+  sp->serialize(writer);
+
+  EXPECT_EQ(4, writer.getByteCount());
+  EXPECT_EQ(0xEF, d[0]);
+  EXPECT_EQ(0xBE, d[1]);
+  EXPECT_EQ(0xAD, d[2]);
+  EXPECT_EQ(0xDE, d[3]);
+}
+
+TEST_F(SerializablePrimitiveTest, test_serialize_EIP_UDINT_accessor)
+{
+  SerializablePrimitive <EIP_UDINT> sp;
+  sp.data = 0xDEADBEEF;
+
+  unsigned char d[4]; 
+  BufferWriter writer(buffer(d));
+
+  sp.serialize(writer);
+
+  EXPECT_EQ(4, writer.getByteCount());
+  EXPECT_EQ(0xEF, d[0]);
+  EXPECT_EQ(0xBE, d[1]);
+  EXPECT_EQ(0xAD, d[2]);
+  EXPECT_EQ(0xDE, d[3]);
+}
+
+TEST_F(SerializablePrimitiveTest, test_serialize_EIP_INT)
+{
+  shared_ptr<Serializable> sp = make_shared< SerializablePrimitive <EIP_INT> > (-1);
+
+  unsigned char d[2]; 
+  BufferWriter writer(buffer(d));
+
+  sp->serialize(writer);
+
+  EXPECT_EQ(2, writer.getByteCount());
+  EXPECT_EQ(0xFF, d[0]);
+  EXPECT_EQ(0xFF, d[1]);
+}
+
+TEST_F(SerializablePrimitiveTest, test_serialize_EIP_UDINT_short_buffer)
+{
+  shared_ptr<Serializable> sp = make_shared< SerializablePrimitive <EIP_UDINT> > (0xDEADBEEF);
+
+  unsigned char d[3]; 
+  BufferWriter writer(buffer(d));
+  ASSERT_THROW(sp->serialize(writer);, std::length_error);
+}
+
+TEST_F(SerializablePrimitiveTest, test_deserialize_EIP_UDINT)
+{
+  shared_ptr<SerializablePrimitive <EIP_UDINT> > sp = make_shared< SerializablePrimitive <EIP_UDINT> > ();
+  shared_ptr<Serializable> s = sp;
+
+  unsigned char d[] = { 0xEF, 0xBE, 0xAD, 0xDE };
+  BufferReader reader(buffer(d));
+
+  s->deserialize(reader);
+
+  EXPECT_EQ(4, reader.getByteCount());
+  EXPECT_EQ(0xDEADBEEF, sp->data);
+}
+
+TEST_F(SerializablePrimitiveTest, test_deserialize_EIP_INT)
+{
+  shared_ptr<SerializablePrimitive <EIP_INT> > sp = make_shared< SerializablePrimitive <EIP_INT> > ();
+  shared_ptr<Serializable> s = sp;
+
+  unsigned char d[] = { 0xFF, 0xFF };
+  BufferReader reader(buffer(d));
+
+  s->deserialize(reader);
+
+  EXPECT_EQ(2, reader.getByteCount());
+  EXPECT_EQ(-1, sp->data);
+}
+
+TEST_F(SerializablePrimitiveTest, test_deserialize_EIP_UDINT_short_buffer)
+{
+  shared_ptr<SerializablePrimitive <EIP_UDINT> > sp = make_shared< SerializablePrimitive <EIP_UDINT> > ();
+  shared_ptr<Serializable> s = sp;
+
+  unsigned char d[] = { 0xEF, 0xBE, 0xAD };
+  BufferReader reader(buffer(d));
+
+  ASSERT_THROW(s->deserialize(reader), std::length_error);
+}
