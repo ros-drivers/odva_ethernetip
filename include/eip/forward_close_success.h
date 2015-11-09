93372bca20ba9a71d87d7a867f26be8f78347904
Kareem Shehata <kshehata@clearpathrobotics.com>
2015-11-09 21:20:08 -0200

    Added class for handling forward close success response It would probably be pretty easy to extend to the failure case if needed

---
+++ include/eip/forward_close_success.h
@@ -0,0 +1,122 @@
+/**
+Software License Agreement (proprietary)
+
+\file      forward_close_success.h
+\authors   Kareem Shehata <kshehata@clearpathrobotics.com>
+\copyright Copyright (c) 2015, Clearpath Robotics, Inc., All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, is not permitted without the
+express permission of Clearpath Robotics.
+*/
+
+#ifndef EIP_FORWARD_CLOSE_SUCCESS_H
+#define EIP_FORWARD_CLOSE_SUCCESS_H
+
+#include <string>
+#include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
+
+#include "eip/eip_types.h"
+#include "eip/serialization/serializable.h"
+#include "eip/serialization/serializable_buffer.h"
+#include "eip/serialization/reader.h"
+#include "eip/serialization/writer.h"
+
+using boost::shared_ptr;
+using boost::make_shared;
+
+namespace eip {
+
+using serialization::Serializable;
+using serialization::SerializableBuffer;
+using serialization::Reader;
+using serialization::Writer;
+
+/**
+ * Class to encapsulate the response to a Forward Close request when successful
+ * Note that the response is different if the request fails.
+ */
+class ForwardCloseSuccess : public Serializable
+{
+public:
+
+  EIP_UINT connection_sn;
+  EIP_UINT originator_vendor_id;
+  EIP_UDINT originator_sn;
+
+  /**
+   * Get the response data object in this message
+   * @return Serializable data to be used for this response
+   */
+  shared_ptr<Serializable> getResponseData() const
+  {
+    return response_data_;
+  }
+
+  /**
+   * Get the length of serialized data that would be produced if serialized
+   * @return Total length in bytes to be serialized
+   */
+  virtual size_t getLength() const
+  {
+    size_t result = sizeof(connection_sn)
+      + sizeof(originator_vendor_id)
+      + sizeof(originator_sn)
+      + 2; // reserved byte and data length
+    if (response_data_)
+    {
+      result += response_data_->getLength();
+    }
+    return result;
+  }
+
+  /**
+   * Serialize data into the given buffer
+   * @param writer Writer to use for serialization
+   * @return the writer again
+   * @throw std::length_error if the buffer is too small for the header data
+   */
+  virtual Writer& serialize(Writer& writer) const
+  {
+    std::logic_error("Not implemented");
+  }
+
+  /**
+   * Don't need extra length info, as the message data defines its length
+   */
+  virtual Reader& deserialize(Reader& reader, size_t length)
+  {
+    deserialize(reader);
+  }
+
+  /**
+   * Deserialize the forward open response data. Length is implicit.
+   */
+  virtual Reader& deserialize(Reader& reader)
+  {
+    EIP_USINT data_length;
+    reader.read(connection_sn);
+    reader.read(originator_vendor_id);
+    reader.read(originator_sn);
+    reader.read(data_length);
+    reader.skip(1);
+    if (data_length > 0)
+    {
+      shared_ptr<SerializableBuffer> sb = make_shared<SerializableBuffer> ();
+      sb->deserialize(reader, data_length*2);
+      response_data_ = sb;
+    }
+    else
+    {
+      response_data_ = shared_ptr<Serializable>();
+    }
+  }
+
+private:
+  shared_ptr<Serializable> response_data_;
+
+};
+
+} // namespace eip
+
+#endif  // EIP_FORWARD_CLOSE_SUCCESS_H
---
+++ test/eip/forward_close_success_test.cpp
@@ -0,0 +1,94 @@
+/**
+Software License Agreement (proprietary)
+
+\file      forward_close_success_test.cpp
+\authors   Kareem Shehata <kshehata@clearpathrobotics.com>
+\copyright Copyright (c) 2015, Clearpath Robotics, Inc., All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, is not permitted without the
+express permission of Clearpath Robotics.
+*/
+
+#include <gtest/gtest.h>
+#include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
+#include <boost/asio.hpp>
+
+#include "eip/forward_close_success.h"
+#include "eip/serialization/serializable_buffer.h"
+#include "eip/serialization/buffer_reader.h"
+#include "eip/serialization/buffer_writer.h"
+
+using boost::shared_ptr;
+using boost::make_shared;
+using namespace boost::asio;
+using eip::ForwardCloseSuccess;
+using eip::serialization::SerializableBuffer;
+using eip::serialization::BufferReader;
+using eip::serialization::BufferWriter;
+
+class ForwardCloseSuccessTest : public :: testing :: Test
+{
+
+};
+
+TEST_F(ForwardCloseSuccessTest, test_deserialize_no_data)
+{
+  EIP_BYTE d[] = {
+    // connection serial number
+    0x55, 0xAA,
+    // originator vendor ID
+    0xA9, 0xCB,
+    // originator serial number
+    0x12, 0x34, 0x55, 0xAA,
+    // reply size
+    0,
+    // reserved byte
+    0xFF,
+  };
+
+  ForwardCloseSuccess data;
+  EXPECT_EQ(sizeof(d), data.getLength());
+  BufferReader reader(buffer(d));
+  data.deserialize(reader, sizeof(d));
+  EXPECT_EQ(sizeof(d), reader.getByteCount());
+
+  EXPECT_EQ(0xAA55, data.connection_sn);
+  EXPECT_EQ(0xCBA9, data.originator_vendor_id);
+  EXPECT_EQ(0xAA553412, data.originator_sn);
+  EXPECT_FALSE(data.getResponseData());
+}
+
+TEST_F(ForwardCloseSuccessTest, test_deserialize_with_data)
+{
+  EIP_BYTE d[] = {
+    // connection serial number
+    0x55, 0xAA,
+    // originator vendor ID
+    0xA9, 0xCB,
+    // originator serial number
+    0x12, 0x34, 0x55, 0xAA,
+    // reply size
+    4,
+    // reserved byte
+    0xFF,
+    // additional data
+    0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xEF,
+  };
+
+  ForwardCloseSuccess data;
+  BufferReader reader(buffer(d));
+  data.deserialize(reader, sizeof(d));
+  EXPECT_EQ(sizeof(d), reader.getByteCount());
+
+  EXPECT_EQ(0xAA55, data.connection_sn);
+  EXPECT_EQ(0xCBA9, data.originator_vendor_id);
+  EXPECT_EQ(0xAA553412, data.originator_sn);
+
+  ASSERT_TRUE(data.getResponseData());
+  shared_ptr<SerializableBuffer> sb = boost::dynamic_pointer_cast<SerializableBuffer>
+    (data.getResponseData());
+  ASSERT_TRUE(sb);
+  EXPECT_EQ(8, sb->getLength());
+  EXPECT_EQ(d + 10, buffer_cast<EIP_BYTE*>(sb->getData()));
+}
