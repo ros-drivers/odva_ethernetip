357d8b79822eb7fc77dc153bba4598f3d07f5bfc
Kareem Shehata <kshehata@clearpathrobotics.com>
2015-11-10 11:38:28 -0200

    Minor cleanup to boilerplate of EIP Session

--- src/eip/session.cpp
+++ src/eip/session.cpp
@@ -1,7 +1,7 @@
 /**
 Software License Agreement (proprietary)
 
-\file      eip_session.cpp
+\file      session.cpp
 \authors   Kareem Shehata <kshehata@clearpathrobotics.com>
 \copyright Copyright (c) 2015, Clearpath Robotics, Inc., All rights reserved.
 
